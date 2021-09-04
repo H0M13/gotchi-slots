2a0de1b4cf778b50e6d39768c40f172c9223a934
MATTOP\Matthew <m.durkin92@gmail.com>
2021-09-04 21:19:44 +0100

    Add logic to fetch spin outcome from smart contract for spinner

--- components/sections/Spinner/SlotMachine.tsx
+++ components/sections/Spinner/SlotMachine.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState, useRef } from "react";
+import { useAavegotchi } from "context/AavegotchiContext";
+import { useSlotsContractCall } from "actions/web3";
 
-import styles from './SlotMachine.module.scss';
+import styles from "./SlotMachine.module.scss";
+import { useMoralis } from "react-moralis";
 
-const RepeatButton = ({onClick}) => (
-  <button 
-      aria-label='Play again.' 
-      className={styles.repeatButton} 
-      onClick={onClick}>
-  </button>
+const RepeatButton = ({ onClick }) => (
+  <button
+    aria-label="Play again."
+    className={styles.repeatButton}
+    onClick={onClick}
+  ></button>
 );
 
 export interface SpinnerProps {
@@ -33,9 +36,9 @@ const useInterval = (callback, delay) => {
       return () => clearInterval(id);
     }
   }, [delay]);
-}
+};
 
-const Spinner = ({id} : SpinnerProps) => {
+const Spinner = ({ id }: SpinnerProps) => {
   const iconHeight = 100;
   const [spinning, setSpinning] = useState(false);
   const [position, setPosition] = useState(100);
@@ -46,69 +49,159 @@ const Spinner = ({id} : SpinnerProps) => {
   const calculateTime = (results) => {
     const resultID = results.result[id];
     const previousResultID = results.previousResult[id];
-    const padding = (id+1) * 1800;
+    const padding = (id + 1) * 1800;
     const distance = resultID - previousResultID;
     let spinTime = 100 * distance;
     return padding + spinTime;
-  }
+  };
 
   const startSpin = (event) => {
-    if(!spinning) {
+    if (!spinning) {
       setSpinning(true);
       const spinTime = calculateTime(event.detail);
       setTimeRemaining(spinTime);
     }
-  }
+  };
 
-  const tick = () => {    
-    if (timeRemaining <= 0) { 
-      setSpinning(false); 
+  const tick = () => {
+    if (timeRemaining <= 0) {
+      setSpinning(false);
     } else {
       setPosition(position - speed);
       setTimeRemaining(timeRemaining - 100);
-    }      
-  }
+    }
+  };
 
   useEffect(() => {
-    window.addEventListener('spin', startSpin)
+    window.addEventListener("spin", startSpin);
     return function cleanupListener() {
-      window.removeEventListener('spin', startSpin)
-    }
+      window.removeEventListener("spin", startSpin);
+    };
   }, []);
 
   useInterval(() => {
-    tick()
+    tick();
   }, 100);
 
   return (
-    <div 
-      style={{backgroundPosition: `0px ${position}px`}}
-      className={styles.icons}          
+    <div
+      style={{ backgroundPosition: `0px ${position}px` }}
+      className={styles.icons}
     />
   );
 };
 
 const SlotMachine = ({ className = "" }: any) => {
-  const [count, setCount] = useState(0);
-  const results = [
-    [2, 2, 2, 2, 2],
-    [3, 3, 3, 3, 3],
+  const payout0Results = [
     [1, 2, 3, 4, 5],
     [5, 4, 3, 2, 1],
-    [6, 6, 6, 6, 6],
-    [16, 4, 16, 4, 16]
   ];
+
+  const payout1Results = [
+    [1, 1, 1, 2, 3],
+    [2, 3, 1, 1, 1],
+  ];
+
+  const payout2Results = [
+    [5, 5, 5, 2, 3],
+    [2, 3, 5, 5, 5],
+  ];
+
+  const payout5Results = [
+    [9, 9, 9, 2, 3],
+    [2, 3, 9, 9, 9],
+  ];
+
+  const payout25Results = [
+    [9, 9, 9, 9, 9],
+    [14, 14, 14, 2, 3],
+    [2, 3, 14, 14, 14],
+    [11, 11, 11, 2, 3],
+    [2, 3, 11, 11, 11],
+  ];
+
+  const payout100Results = [
+    [14, 14, 14, 14, 14],
+    [11, 11, 11, 11, 3],
+    [2, 11, 11, 11, 11],
+    [2, 3, 16, 16, 16],
+    [16, 16, 16, 2, 3],
+  ];
+
+  const payoutJackpotResults = [[16, 16, 16, 16, 16]];
+
+  const getRandomEntryFromArray = (array) =>
+    array[Math.floor(Math.random() * array.length)];
+
+  const getRandomSpinResultForPayout = (spinOutcome: number) => {
+    console.log(spinOutcome)
+    switch (+spinOutcome) {
+      case 0:
+        return getRandomEntryFromArray(payout0Results);
+      case 1:
+        return getRandomEntryFromArray(payout1Results);
+      case 2:
+        return getRandomEntryFromArray(payout2Results);
+      case 5:
+        return getRandomEntryFromArray(payout5Results);
+      case 25:
+        return getRandomEntryFromArray(payout25Results);
+      case 100:
+        return getRandomEntryFromArray(payout100Results);
+      default:
+        return getRandomEntryFromArray(payoutJackpotResults);
+    }
+  };
+
+  const {
+    state: { requestId },
+    dispatch,
+  } = useAavegotchi();
+
+  const { web3 } = useMoralis();
+
+  const [currentSpinIndex, setCurrentSpinIndex] = useState<number>(0);
+
+  const getSpinOutcome = async (requestId: string, spinIndex: number) => {
+    const res = await useSlotsContractCall<string>(web3, {
+      name: "requestIdToSpinOutcomes",
+      parameters: [requestId, spinIndex],
+    });
+    console.log(`spin ${spinIndex}: ${web3.utils.fromWei(res)}`);
+    return web3.utils.fromWei(res);
+  };
 
   const [previousResult, setPreviousResult] = useState([1, 1, 1, 1, 1]);
 
-  const spin = () => {
-    const event = new CustomEvent('spin', { 'detail': {
-      previousResult: previousResult,
-      result: results[count],
-    } });
+  useEffect(() => {
+    console.log("requestId has changed. Setting currentSpinIndex to 0");
+    setCurrentSpinIndex(0);
+  }, [requestId]);
+
+  const spin = async () => {
+    if (!requestId) {
+      console.log("Tried to spin but requestId is undefined");
+      return;
+    }
+
+    if (currentSpinIndex > 9) {
+      console.log("User is out of spins");
+      return;
+    }
+
+    const spinOutcome = await getSpinOutcome(requestId, currentSpinIndex);
+
+    const spinResult = getRandomSpinResultForPayout(spinOutcome);
+
+    const event = new CustomEvent("spin", {
+      detail: {
+        previousResult: previousResult,
+        result: spinResult,
+      },
+    });
     window.dispatchEvent(event);
-    setCount((count + 1) % results.length);
-    setPreviousResult(results[count]);
+    setCurrentSpinIndex(currentSpinIndex + 1);
+    setPreviousResult(spinResult);
   };
 
   return (
@@ -122,10 +215,9 @@ const SlotMachine = ({ className = "" }: any) => {
         <div className="gradient-fade"></div>
       </div>
 
-      <RepeatButton onClick={() => spin()}/>       
+      <RepeatButton onClick={() => spin()} />
     </div>
   );
-}; 
-
+};
 
 export default SlotMachine;
